3ac1e234dc9d8435c1586cb3b2d4044a32f60361
branden higby <branden@higby.io>
2024-11-17 08:16:44 -0800

    Update author.js

--- source/data/author.js
+++ source/data/author.js
@@ -4,5 +4,5 @@ export default {
 	email: "branden@higby.io",
 	github: "https://github.com/higby/",
 	mastodon: "https://front-end.social/@higby",
-	letterboxd: "https://letterboxd.com/hugabee"
+	letterboxd: "https://letterboxd.com/higby"
 }
