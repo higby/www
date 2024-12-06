48e7776dc83eb5490411bd5cd29bdeb8c798ea45
branden higby <branden@higby.io>
2024-12-06 08:47:41 -0800

    Add attribution markdown-it plugin

--- config/markdown.js
+++ config/markdown.js
@@ -1,14 +1,21 @@
 import md from "markdown-it"
+import mdAttribution from "markdown-it-attribution"
 import mdAttrs from "markdown-it-attrs"
 import mdDefList from "markdown-it-deflist"
 import mdImplicitFigures from "markdown-it-implicit-figures"
 
+//
 const markdown = md({
 	html: true,
 	breaks: true,
 	linkify: true,
 	typographer: true
 })
+	.use(mdAttribution, {
+		classNameContainer: "",
+		classNameAttribution: "",
+		removeMarker: false
+	})
 	.use(mdAttrs)
 	.use(mdDefList)
 	.use(mdImplicitFigures)
